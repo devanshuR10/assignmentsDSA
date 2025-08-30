0873112b846460b8c8ad8421520c770ed42e5af6
devanshuR10 <devanshur583@gmail.com>
2025-08-30 22:52:48 +0530

    Create lower_triangle_matrix.cpp

---
+++ lower_triangle_matrix.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[3];  
+    
+    cout << "Enter three diagonal elements: ";
+    for(int i = 0; i < 3; i++){
+        cin >> arr[i];
+    }
+
+    cout << "\nDiagonal Matrix (3x3):" << endl;
+    for(int i = 0; i < 3; i++){
+        for(int j = 0; j < 3; j++){
+            if(i > j){
+                cout << arr[i] << " ";
+            } else {
+                cout << "0 ";
+            }
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
